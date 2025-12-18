2a2072c9f4441bdd3af29a8656cab711a488209b
git10a <tomtom211997@gmail.com>
2025-12-18 09:47:00 +0900

    Add www to non-www 301 redirect for SEO canonical URL consolidation

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com', 'images.microcms-assets.io'],
     },
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.hinode-run.com',
+                    },
+                ],
+                destination: 'https://hinode-run.com/:path*',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
