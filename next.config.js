f494e58314f5f14accdc71c43bbfb97c0333b4fa
git10a <tomtom211997@gmail.com>
2025-12-18 13:04:02 +0900

    Remove www redirect from next.config.js - use Vercel redirects

--- next.config.js
+++ next.config.js
@@ -3,21 +3,6 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com', 'images.microcms-assets.io'],
     },
-    async redirects() {
-        return [
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: 'www.hinode-run.com',
-                    },
-                ],
-                destination: 'https://hinode-run.com/:path*',
-                permanent: true,
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
