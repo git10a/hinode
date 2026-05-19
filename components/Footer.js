5ef7bb8fa851c08dde6b099e939939a50ff797ce
git10a <tomtom211997@gmail.com>
2026-05-19 15:32:49 +0900

    Remove store link from footer

--- components/Footer.js
+++ components/Footer.js
@@ -56,11 +56,6 @@ export default function Footer() {
                             {socialIconByKey[link.key]}
                         </a>
                     ))}
-                    <a href="https://hinoderun.stores.jp/" target="_blank" rel="noopener noreferrer" aria-label="Shop">
-                        <svg className="social-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4H6zM3.5 6h17M16 10a4 4 0 0 1-8 0" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
-                    </a>
                 </div>
                 <p>&copy; {currentYear} HINODE All rights reserved.</p>
             </div>
